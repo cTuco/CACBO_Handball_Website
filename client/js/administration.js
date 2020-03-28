906f86c06fa90532005d6ee941cf478831ed2e60
Clara TUCO <ctuco@ensc.fr>
2020-03-28 12:10:37 +0100

    affichage des elements propre à l'équipe (en dur)

--- client/js/administration.js
+++ client/js/administration.js
@@ -15,6 +15,15 @@ Template.administration.events({
 
 Template.calendrierResultats.helpers({
     params() {
-        return FlowRouter.getParam('params');
+        //TODO : Récupérer dans la bdd les infos relatives aux équipes
+        const param = FlowRouter.getParam('params');
+        var titre = '';
+        if (param === 'SG') {
+            titre = 'Séniors Garçons';
+        }
+        if (param === 'SF') {
+            titre = 'Séniors Filles';
+        }
+        return titre;
     },
 })
