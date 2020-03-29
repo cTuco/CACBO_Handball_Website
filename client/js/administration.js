28293993feaeb2359991f42bd9c4bfb8eaa20437
Clara TUCO <ctuco@ensc.fr>
2020-03-29 11:22:54 +0200

    amélio code

--- client/js/administration.js
+++ client/js/administration.js
@@ -1,15 +1,7 @@
 Template.administration.events({
     'click .btn': function () {
         const idElement = event.target.id;
-        if (idElement == 'SG') {
-            FlowRouter.go('administrationParams', { 'params': idElement })
-            //afficher template de modification des résultats et du calendrier
-        }
-        if (idElement == 'SF') {
-            FlowRouter.go('administrationParams', { 'params': idElement })
-            //FlowRouter.go('calendrier&resultats');
-            //afficher template de modification des résultats et du calendrier
-        }
+        FlowRouter.go('administrationParams', { 'params': idElement })
     }
 })
 
