9c90ca2cdc5fb122835ecabb42fb46ebab1e9538
Clara TUCO <ctuco@ensc.fr>
2020-04-16 09:35:05 +0200

    matchs passe/ a venir actualite

--- client/js/actualite.js
+++ client/js/actualite.js
@@ -8,9 +8,37 @@ Template.actualite.onCreated(function () {
     })
 })
 
-Template.actualite.helpers({
+Template.tableauMatchResultatActualiteAVenir.helpers({
     //afficher tous les matchs de toutes les équipes
-    matchs() {
-        return Equipes.find({})//TODO : à changer !!
+    matchsAVenir() {
+        console.log("matchsAVenir")
+        const Equipe = Equipes.find({});
+        let matchsAVenir = [];
+
+        Equipe.forEach(element => {
+            element.matchs.forEach(match => {
+                if (match.passe === false) {
+                    matchsAVenir.push(match);
+                }
+            });
+        });
+        return matchsAVenir;
+    },
+});
+
+Template.tableauMatchResultatActualitePasse.helpers({
+    //afficher tous les matchs de toutes les équipes
+    matchsPasse() {
+        const Equipe = Equipes.find({});
+        let matchsPasse = [];
+
+        Equipe.forEach(element => {
+            element.matchs.forEach(match => {
+                if (match.passe === true) {
+                    matchsPasse.push(match);
+                }
+            });
+        });
+        return matchsPasse;
     },
 });
