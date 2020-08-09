ed368a3d4eb06ee26af26afbc3bab216a466b38d
Clara TUCO <ctuco@ensc.fr>
2020-08-09 14:55:15 +0200

    supprimer partenaire

--- client/js/administration.js
+++ client/js/administration.js
@@ -1,7 +1,13 @@
-//Au clic sur une des équipes, on récupère l'id pour le transmettre dans la route
+//Au clic sur un bouton, on récupère l'id pour le transmettre dans la route
 Template.administration.events({
     'click .btn': function () {
         const idElement = event.target.id;
-        FlowRouter.go('administrationParams', { 'params': idElement })
+        //si l'id est une équipe
+        console.log(idElement)
+        if (idElement !== "nouveauPartenaire" && idElement !== "modifierPartenaire" && idElement !== "supprimerPartenaire") {
+            FlowRouter.go('administrationParams', { 'params': idElement });
+        } else {
+            console.log('partenaire gro')
+        }
     },
-})
+});
--- client/js/partenaires.js
+++ client/js/partenaires.js
@@ -1,17 +1,63 @@
 import { Meteor } from 'meteor/meteor';
 
-Template.partenairesAdmin.events({
+Template.partenaires.onCreated(function () {
+    var self = this;
+    self.autorun(function () {
+        self.subscribe('partenaires');
+    });
+});
+
+Template.modalPartenaire.events({
     //ouverture et fermture des modal
-    'click .btnPartenaire': function () {
-        $('#modalMatch').addClass("visible");
-        $('#modalMatch').toggle("visible");
+    'submit .btn-primary': function () {
+        $('#modalPartenaire').removeClass("visible");
+        $('#modalPartenaire').toggle("visible");
     },
-    // 'submit .btn-primary': function () {
-    //     $('#modalMatch').removeClass("visible");
-    //     $('#modalMatch').toggle("visible");
-    // },
-    // 'click #annuler': function () {
-    //     $('#modalMatch').removeClass("visible");
-    //     $('#modalMatch').toggle("visible");
-    // },
+    'click #annulerPartenaire': function () {
+        console.log('annuler');
+        $('#modalPartenaire').removeClass("visible");
+        $('#modalPartenaire').toggle("visible");
+    },
+});
+
+//pour ouvrir la modal d'insertion
+Template.partenairesAdmin.events({
+    'click #nouveauPartenaire': function () {
+        $('#modalPartenaire').addClass("visible");
+        $('#modalPartenaire').toggle("visible");
+    },
+    //ouverture et fermture des modal modification partenaire
+    'click .waves-effect': function () {
+        $('#modalModifierPartenaire').addClass("visible");
+        $('#modalModifierPartenaire').toggle("visible");
+    },
+    'submit .btn-primary': function () {
+        $('#modalPartenaire').removeClass("visible");
+        $('#modalPartenaire').toggle("visible");
+    },
+    'click #annulerPartenaire': function () {
+        console.log('annuler');
+        $('#modalPartenaire').removeClass("visible");
+        $('#modalPartenaire').toggle("visible");
+    }
+});
+
+Template.tableauPartenairesAdmin.events({
+    //supprimer un partenaire
+    'click #supprimerPartenaire': function () {
+        const partenaire = Partenaires.findOne({ _id: this._id });
+        Meteor.call('supprimerPartenaire', partenaire);
+    },
+});
+
+//permet d'afficher les infos relatives aux partenaires
+Template.partenaires.helpers({
+    partenaires() {
+        return Partenaires.find({});
+    }
+});
+Template.partenairesAdmin.helpers({
+    partenaires() {
+        return Partenaires.find({});
+    }
 });
